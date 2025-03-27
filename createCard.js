6bb5f45a4b30fb5ed960b458a6d4a663cdf0819f
CAYUELA Annick <annickcayuela.ac@gmail.com>
2025-03-27 09:44:05 +0100

    ModificationCardandHTML

---
+++ createCard.js
@@ -0,0 +1,93 @@
+function createCard(restaurant) {
+    
+    // Création des éléments de la carte
+    
+const newCard = document.createElement("div")
+const newCardHeader = document.createElement("div")
+const newCardImg = document.createElement("div")
+
+const newCardBody = document.createElement("div")
+const newCardTitle = document.createElement("h3")
+const newCardDescription = document.createElement("p")
+const newCardFeatures = document.createElement("div")
+
+const newCardDistance = document.createElement("span")
+const newCardTitleDistance = document.createElement("h4")
+const newCardTextDistance = document.createElement("p")
+
+const newCardPrice = document.createElement("span")
+const newCardTitlePrice = document.createElement("h4")
+const newCardTextPrice = document.createElement("p")
+
+const newCardCategory = document.createElement("span")
+const newCardTitleCategory = document.createElement("h4")
+const newCardTextCategory = document.createElement("p")
+
+const newCardButton = document.createElement("button")
+
+// Ajout des classes des éléments respectifs
+newCard.classList.add("Restaurant")
+newCardHeader.classList.add("card-header")
+newCardImg.classList.add("card-img")
+
+newCardBody.classList.add("card-body")
+newCardTitle.classList.add("card-title")
+newCardDescription.classList.add("card-description")
+newCardFeatures.classList.add("card-features")
+newCardButton.classList.add("card-button")
+
+newCardDistance.classList.add("card-distance")
+newCardTitleDistance.classList.add("card-title-item")
+newCardTextDistance.classList.add("card-title-text")
+
+newCardPrice.classList.add("card-price")
+newCardTitlePrice.classList.add("card-title-item")
+newCardTextPrice.classList.add("card-title-text")
+
+newCardCategory.classList.add("card-category")
+newCardTitleCategory.classList.add("card-title-item")
+newCardTextCategory.classList.add("card-title-text")
+
+
+
+// Gestion du contenu des éléments à partir de l'objet Restaurant
+newCardImg.src = 
+newCardTitle.textContent = 
+newCardDescription.textContent =
+
+newCardTitleDistance.textContent =
+newCardTextDistance.textContent =
+
+newCardTitlePrice.textContent =
+newCardTextPrice.textContent =
+
+newCardTitleCategory.textContent =
+newCardTextCategory.textContent =
+
+newCardButton.textContent = "Lien Google"
+
+// Placements des éléments avec leur parent respectif
+newCard.appendChild(newCardHeader)
+newCard.appendChild(newCardBody)
+
+newCardHeader.appendChild(newCardImg)
+
+newCardBody.appendChild(newCardTitle)
+newCardBody.appendChild(newCardDescription)
+newCardBody.appendChild(newCardFeatures)
+newCardBody.appendChild(newCardButton)
+
+newCardDistance.appendChild(newCardTitleDistance)
+newCardDistance.appendChild(newCardTextDistance)
+
+newCardPrice.appendChild(newCardTitlePrice)
+newCardPrice.appendChild(newCardTextPrice)
+
+newCardCategory.appendChild(newCardTitleCategory)
+newCardCategory.appendChild(newCardTextCategory)
+
+// On retourne la carte unique correspondant à l'objet passé en paramètre
+return newCard
+}
+
+export default createCard;
