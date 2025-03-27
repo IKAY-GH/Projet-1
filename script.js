b2fdfbab3fa2bb7db7162554f56f2e1d33eb6420
first name last_name <ridouane@MacBook-Air-de-ridouane.local>
2025-03-27 11:06:40 +0100

    commit before merge

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-const menuHamburger = document.querySelector(".menu_hamburger")
-const navLinks = document.querySelector(".nav_links")
+const restaurants = [
+  { id: 1, type: "kebab", rating: 4.5, reviews: 120 },
+  { id: 2, type: "Asiatique", rating: 4.8, reviews: 95 },
+  { id: 3, type: "Végé", rating: 4.2, reviews: 150 },
+  { id: 4, type: "Marocaine", rating: 4.7, reviews: 80 },
+  { id: 5, type: "Street Food", rating: 4.7, reviews: 80 },
+  
 
-menuHamburger.addEventListener('click',()=>{
-    navLinks.classList.toggle('mobile-menu')
-})
+];
+
+const category = document.querySelector(".Catégorie")
+
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.getElementById("restaurants-container");
+  
+  restaurants.forEach((restaurant) => {
+    const restaurantDiv = document.createElement("div");
+    restaurantDiv.classList.add("restaurant-card");
+    
+    restaurantDiv.innerHTML = `
+      <h3>${restaurant.name}</h3>
+      <p>⭐ ${restaurant.rating} (${restaurant.reviews} avis)</p>
+    `;
+    
+    container.appendChild(restaurantDiv);
+  });
+});
+
+document.querySelectorAll(".select-btn").forEach(button => {
+    button.addEventListener("click", function () {
+      const chosenId = parseInt(this.getAttribute("data-id"));
+      const chosenRestaurant = restaurants.find(el => el.id === chosenId);
+      console.log("Restaurant sélectionné:", chosenRestaurant);
+      alert(`Vous avez sélectionné : ${chosenRestaurant.name} - Note: ${chosenRestaurant.rating}`);
+    });
+  });
+
+
